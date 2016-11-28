a0d47e8c95ec7aad0a9fdc7f344f1e32afe2e683
marcioaleson <marcioaleson@gmail.com>
2016-11-27 23:35:31 -0200

    Operações em PHP.

---
+++ operators/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <title>Tipo de Operações em PHP</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+      <?php
+      //Variáveis
+      $x = 10;
+      $y = 6;
+      echo "A soma de".$x."+"."$y"."=".($x+$y);
+      echo "</br>";
+       ?>
+  <!-- JQuery -->
+  <script src="js/jquery-3.1.0.min.js"></script>
+  </body>
+</html>
