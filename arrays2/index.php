a78a5a4291193a166e95b61c2a3cfd755b0366dd
Marcio Aleson <Marcio Aleson>
2017-01-01 03:38:53 -0200

    Array multidimensional in PHP.

---
+++ arrays2/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <title>Operações com arrays em PHP</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+      <?php
+      $cars = array(
+        array("Volvo", 22, 18),
+        array("BMW", 15, 13),
+        array("Saab", 5, 2),
+        array("Land Rover", 17, 15)
+      );
+      echo $cars[0][0]."In stock: ".$cars[0][1].", sold: ".$cars[0][2]."<br>";
+      echo $cars[1][0]."In stock: ".$cars[1][1].", sold: ".$cars[1][2]."<br>";
+      echo $cars[2][0]."In stock: ".$cars[2][1].", sold: ".$cars[2][2]."<br>";
+      echo $cars[3][0]."In stock: ".$cars[3][1].", sold: ".$cars[3][2]."<br>";
+
+
+
+?>
+  <!-- JQuery -->
+  <script src="js/jquery-3.1.0.min.js"></script>
+  </body>
+</html>
