0123ee095a8d5137afb15639afab1ec48d679855
marcioaleson <marcioaleson@gmail.com>
2017-02-19 21:28:55 -0300

    MySQL and PHP example.

---
+++ mysql/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <title>Operações com o MySQL em PHP.</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen,projection">
+  </head>
+  <body>
+  <?php
+  //Variáveis
+?>
+  <!-- JQuery -->
+  <script src="js/jquery-3.1.0.min.js"></script>
+  </body>
+</html>
