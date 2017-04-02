51604c21c55265ae2915b3d5d6e4a075649523d7
marcioaleson <marcioaleson@gmail.com>
2017-04-02 17:14:25 -0300

    PHP last record ID.

---
+++ mysql10/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <title>Operações com o MySQL em PHP.</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen,projection">
+  </head>
+  <body>
+    <?php
+    $servername = "localhost:3303";
+    $username = "root";
+    $password = "";
+    $dbname = "test";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+        VALUES ('John', 'Doe', 'john@example.com')";
+        // use exec() because no results are returned
+        $conn->exec($sql);
+        echo "New record created successfully";
+        }
+    catch(PDOException $e)
+        {
+        echo $sql . "<br>" . $e->getMessage();
+        }
+
+    $conn = null;
+?>
+    ?>
+  <!-- JQuery -->
+  <script src="js/jquery-3.1.0.min.js"></script>
+  </body>
+</html>
--- mysql9/index.php
+++ mysql9/index.php
@@ -13,22 +13,24 @@
     $password = "";
     $dbname = "test";
 
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "INSERT INTO MyGuests (firstname, lastname, email)
-        VALUES ('John', 'Doe', 'john@example.com')";
-        // use exec() because no results are returned
-        $conn->exec($sql);
-        echo "New record created successfully";
-        }
-    catch(PDOException $e)
-        {
-        echo $sql . "<br>" . $e->getMessage();
-        }
+   //Create connection
+   $conn = new mysqli($servername, $username, $password, $dbname);
+   //Check connection
+   if ($conn -> connect_error) {
+     # code...
+     die("Connection failed: " . $conn -> connection_error);
+   }
 
-    $conn = null;
+   $sql = "ISERT INTO MyGuests (firstname, lastname, email) VALUES ('John', 'Doe', 'john@example.com')";
+
+   if ($conn -> query($sql) === TRUE) {
+     # code...
+     $last_id = $conn -> insert_id;
+     echo "New record created successfully. Last inserted ID is: ".$last_id;
+   }else {
+     # code...
+     echo "Error: ".$sql."<br>".$conn->error;
+   }
 ?>
     ?>
   <!-- JQuery -->
