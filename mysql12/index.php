94188163a973f8b88bb1fd2fed69614567793cce
marcioaleson <marcioaleson@gmail.com>
2017-05-07 20:48:21 -0300

    PHP insert multiple records into mysql.

--- mysql12/index.php
+++ mysql12/index.php
@@ -13,20 +13,6 @@
     $password = "";
     $dbname = "test";
 
-    try {
-      $conn = new PDO("mysql:host=$servername; dbname=$dbname", $uesrname, $password);
-      //Set the PDO error mode to exception
-      $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $sql = "INSERT INTO MyGuests (firstname, lastname, email) VALUES ('john', 'Doe', 'john@example.com')";
-      //Use exec() because no results are returned
-      $conn -> exec($sql);
-      $last_id = $conn -> lastInserted();
-      echo "New record created successfully. Last inserted ID is: ".$last_id;
-    } catch (Exception $e) {
-      echo $sql . "<br>" . $e -> getMessage();
-    }
-
-    $conn = null;
 
 ?>
     ?>
