29d838c3ab0ed6183896c8324c3ae0663c889735
marcioaleson <marcioaleson@gmail.com>
2017-08-13 20:55:40 -0300

    MySQLi with Prepared Statement.

--- mysql16/index.php
+++ mysql16/index.php
@@ -12,8 +12,35 @@
     $username = "root";
     $password = "";
     $dbname = "test";
+    //Example with MySQLi
+    //Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
+    //Check connection
+    if($conn -> connect_error){
+      die("Connection failed: " . $conn -> connect_error);
+    }
 
+    //Prepare and bind
+    $stmt = $conn -> prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $firstname, $lastname, $email);
+
+    //Set parameters and execute
+
+    $firstname = "John";
+    $lastname = "Doe";
+    $email = "john@example.com";
+    $stmt->execute();
+
+    $firstname = "Mary";
+    $lastname = "Doe";
+    $email = "mary@example.com";
+    $stmt->execute();
+
+    echo "New records created successfylly";
+
+    $stmt->close();
+    $conn->close();
     ?>
   <!-- JQuery -->
   <script src="js/jquery-3.1.0.min.js"></script>
