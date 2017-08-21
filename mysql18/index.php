1198f922763a2557f1eccd87aa905001d2321d81
marcioaleson <marcioaleson@gmail.com>
2017-08-20 23:48:59 -0300

    PHP Select with mysqli example.

--- mysql18/index.php
+++ mysql18/index.php
@@ -13,7 +13,24 @@
     $password = "";
     $dbname = "test";
     //Example with MySQLi
+    //Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    //Check connection
+    if($conn->connect_error){
+      die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "SELECT id, firstname, lastname FROM MyGuests";
+    $result = $conn->query($sql);
 
+    if($result->num_rows > 0){
+      //Output data of each row
+      while($row = $result->fetch_assoc()){
+        echo "Id: ".$row["id"]." - Name: ". $row["firstname"]. " ". $row["lastname"]. " <br>";
+      }
+    }else{
+      echo "0 results ...";
+    }
+    $conn->close();
           ?>
   <!-- JQuery -->
   <script src="js/jquery-3.1.0.min.js"></script>
