6255371574fc6ceaac9fff2398d4eaa009fbbb4e
marcioaleson <marcioaleson@gmail.com>
2017-09-17 20:55:25 -0300

    Deleting records in PHP with MySQLi Object-oriented.

--- mysql20/index.php
+++ mysql20/index.php
@@ -12,23 +12,7 @@
     $username = "root";
     $password = "";
     $dbname = "test";
-
-    //Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    //Check connection
-    if($conn -> connect_error){
-      die("Connection failed: ".$conn->connect_error);
-    }
-
-    //sql to delete a record
-    $sql = "DELETE FROM MyGuests WHERE id=3";
-
-    if($conn->query($sql) == TRUE){
-      echo "Record deleted successfully";;
-    }else{
-      echo "Error deleting record: ".$conn->error;
-    }
-    $conn->close();
+  
           ?>
   <!-- JQuery -->
   <script src="js/jquery-3.1.0.min.js"></script>
