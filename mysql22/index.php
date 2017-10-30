b2bda8a0f5ff355ee5d1b9b9b0fb3b3312a8b13e
marcioaleson <marcioaleson@gmail.com>
2017-10-29 23:34:27 -0200

    PHP MySQL Update Example with MySQLi Object-oriented.

--- mysql22/index.php
+++ mysql22/index.php
@@ -12,8 +12,25 @@
     $username = "root";
     $password = "";
     $dbname = "test";
+    //Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    //Check connection
+    if ($conn->connect_error) {
+      # code...
+      die("Connection failed: ".$conn->connect_error);
+    }
 
-    
+    $sql = "UPDATE MyGuest SET lastname-'Doe' WHERE id=2";
+
+    if ($conn->query($sql) == TRUE) {
+      # code...
+      echo "Record updated successfully";
+    } else {
+      # code...
+      echo "Error updating record: ".$conn->error;
+    }
+
+    $conn->close();
           ?>
   <!-- JQuery -->
   <script src="js/jquery-3.1.0.min.js"></script>
