f98133fb83ebc9f71d23db51fb5aa104a3019669
marcioaleson <marcioaleson@gmail.com>
2017-12-31 23:42:37 -0200

    PHP XML parse Get Elements exemple.

---
+++ phpxml3/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <title>Operações com XML e PHP.</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen,projection">
+  </head>
+  <body>
+    <?php
+       $xml = simplexml_load_file("book.xml") or die("Error: Connot create object");
+       echo $xml->book[0]->title . "<br>";
+       echo $xml->book[1]->title;
+          ?>
+  <!-- JQuery -->
+  <script src="js/jquery-3.1.0.min.js"></script>
+  </body>
+</html>
