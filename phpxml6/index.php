dd7131cea61da1550e9624d2d1d9c01b870251f6
marcioaleson <marcioaleson@gmail.com>
2017-12-31 23:55:39 -0200

    PHP XML parse get attribute values with loop.

---
+++ phpxml6/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <title>Operações com XML e PHP.</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen,projection">
+  </head>
+  <body>
+    <?php
+       $xml = simplexml_load_file("book.xml") or die("Error: Connot create object");
+       foreach ($xml->children() as $books) {
+         # code...
+         echo $books->title['lang'];
+         echo "<br>";
+       }
+          ?>
+  <!-- JQuery -->
+  <script src="js/jquery-3.1.0.min.js"></script>
+  </body>
+</html>
