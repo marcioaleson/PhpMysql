0cb301b76433506f6ec3cedd394b12b19e6d6bbb
marcioaleson <marcioaleson@gmail.com>
2018-01-22 00:23:45 -0200

    PHP and XML Example.

---
+++ phpxml7/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <title>Operações com XML e PHP.</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen,projection">
+  </head>
+  <body>
+    <?php
+
+          ?>
+  <!-- JQuery -->
+  <script src="js/jquery-3.1.0.min.js"></script>
+  </body>
+</html>
