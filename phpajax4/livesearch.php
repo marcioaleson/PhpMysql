bbb605a113c97a6345c8ffc0ab36d96f4b91591b
marcioaleson <marcioaleson@gmail.com>
2018-03-05 01:01:59 -0300

    PHP AJAX and XML Example.

---
+++ phpajax4/livesearch.php
@@ -0,0 +1,42 @@
+<?php
+$xmlDoc = new DOMDocument();
+$xmlDoc -> load("links.xml");
+$x = $xmlDoc->getElementsByTagName("link");
+//get the q parameter from URL
+$q = $_GET["q"];
+//lookup all links from xml file if length of q>0
+if(strlen($q)>0){
+  $hint="";
+  for ($i=0; $i < ($x->length); $i++) {
+    # code...
+    $y = $x->item($i)->getElementsByTagName('title');
+    $z = $x->item($i)->getElementsByTagName('url');
+    if ($y->item(0)->nodeType==1) {
+      //find a link matching the search text
+      if (stristr($y->item(0)->childNodes->item(0)->nodeValue, $q)) {
+        if ($hint == "") {
+          $hint="<a href='" .
+          $z->item(0)->childNodes->item(0)->nodeValue .
+          "' target='_blank'>" .
+          $y->item(0)->childNodes->item(0)->nodeValue . "</a>";
+        } else {
+          $hint=$hint . "<br /><a href='" .
+          $z->item(0)->childNodes->item(0)->nodeValue .
+          "' target='_blank'>" .
+          $y->item(0)->childNodes->item(0)->nodeValue . "</a>";
+        }
+      }
+    }
+  }
+}
+//Set output to "no suggestion" if no hint was found
+// or to correct values
+if ($hint == "") {
+  $response = "No suggestion";
+}else{
+  $response = $hint;
+}
+
+//Output the response
+echo $response;
+ ?>
